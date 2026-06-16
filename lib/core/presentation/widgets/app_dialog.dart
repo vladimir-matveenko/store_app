f67f1798a61f2b9a4596c9c9f4639833003e6971
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-06-16 18:50:59 +0300

    app dialog changed

--- lib/core/presentation/widgets/app_dialog.dart
+++ lib/core/presentation/widgets/app_dialog.dart
@@ -13,33 +13,52 @@ class AppDialog {
     final textTheme = theme.textTheme;
     final result = await showDialog<bool>(
       context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title, style: textTheme.titleMedium),
-        content: Text(text, style: textTheme.bodyMedium),
-        actions: [
-          TextButton(
-            style: TextButton.styleFrom(backgroundColor: Colors.grey),
-            onPressed: () => context.pop(false),
-            child: Text(
-              cancelText,
-              style: textTheme.bodyLarge?.copyWith(
-                color: theme.colorScheme.onPrimary,
+      builder: (context) => Dialog(
+        constraints: const BoxConstraints(maxWidth: 400.0),
+        child: Padding(
+          padding: const .all(16.0),
+          child: Column(
+            mainAxisSize: .min,
+            spacing: 16.0,
+            children: [
+              Text(title, style: textTheme.titleMedium),
+              Text(text, style: textTheme.bodyMedium),
+              Row(
+                mainAxisAlignment: .spaceBetween,
+                mainAxisSize: .min,
+                spacing: 16.0,
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(backgroundColor: Colors.grey),
+                      onPressed: () => context.pop(false),
+                      child: Text(
+                        cancelText,
+                        style: textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.onPrimary,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: theme.colorScheme.error,
+                      ),
+                      onPressed: () => context.pop(true),
+                      child: Text(
+                        okText,
+                        style: textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.onPrimary,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
+            ],
           ),
-          TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: theme.colorScheme.error,
-            ),
-            onPressed: () => context.pop(true),
-            child: Text(
-              okText,
-              style: textTheme.bodyLarge?.copyWith(
-                color: theme.colorScheme.onPrimary,
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
 
