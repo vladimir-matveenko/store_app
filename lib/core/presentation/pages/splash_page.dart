26d4b53796fe9ebc8f7c985bbe95d15f56a24708
Vladimir Matveenko <matvej83@gmail.com>
2026-09-14 18:59:29 +0300

    splash changed

--- lib/core/presentation/pages/splash_page.dart
+++ lib/core/presentation/pages/splash_page.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app/app/constants/app_strings.dart';
 import 'package:store_app/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:store_app/features/auth/presentation/bloc/auth_event.dart';
 
+import '../../../app/utils/utils.dart';
+import '../../../features/auth/presentation/bloc/auth_event.dart';
 import '../widgets/infinite_scroll_background.dart';
 
 class SplashPage extends StatefulWidget {
@@ -15,13 +18,24 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   late AuthBloc authBloc;
+  bool _imagesReady = false;
 
   @override
   void initState() {
     super.initState();
     authBloc = context.read<AuthBloc>();
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(const Duration(seconds: 1), () {
+      AppUtils.precacheImages(
+        context,
+        images: [AppStrings.wall, AppStrings.cart],
+        onDone: () {
+          setState(() {
+            _imagesReady = true;
+          });
+        },
+      );
+      Future.delayed(const Duration(seconds: 2), () {
         authBloc.add(const AuthCheckRequested());
       });
     });
@@ -29,15 +43,17 @@ class _SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: InfiniteScrollingBackground(
-          animationHeight: 250.0,
-          foregroundImageSize: 160.0,
-          backgroundImage: AppStrings.wall,
-          foregroundImage: AppStrings.cart,
-        ),
-      ),
+    return Scaffold(
+      body: _imagesReady
+          ? const Center(
+              child: InfiniteScrollingBackground(
+                animationHeight: 250.0,
+                foregroundImageSize: 160.0,
+                backgroundImage: AppStrings.wall,
+                foregroundImage: AppStrings.cart,
+              ),
+            )
+          : const SizedBox(),
     );
   }
 }
